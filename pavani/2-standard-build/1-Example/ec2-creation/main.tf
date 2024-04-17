bd5ad1cf2e8deb173d627052cc6175cbe54a7699
ranjeetajain <118739251+ranjeetajain@users.noreply.github.com>
2024-04-17 15:13:57 +0530

    Update main.tf

--- pavani/2-standard-build/1-Example/ec2-creation/main.tf
+++ pavani/2-standard-build/1-Example/ec2-creation/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ec2_instance" {
-    ami = "ami-0b0dcb5067f052a63"
+    ami = "ami-0cd59ecaf368e5ccf"
     instance_type = "t2.micro"
     vpc_security_group_ids = [var.SG]
     tags = {Name = "HTTP_SERVER"}
-    key_name = "devops"
+    key_name = "rj"
     count = 1
 }
