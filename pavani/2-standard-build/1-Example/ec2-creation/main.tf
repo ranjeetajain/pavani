23de84e35912b69d4a3796daf9217537e20bcb7a
ranjeetajain <118739251+ranjeetajain@users.noreply.github.com>
2024-04-18 19:48:40 +0530

    Update main.tf

--- pavani/2-standard-build/1-Example/ec2-creation/main.tf
+++ pavani/2-standard-build/1-Example/ec2-creation/main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "ec2_instance" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [var.SG]
     tags = {Name = "HTTP_SERVER"}
-    key_name = "rj"
+    key_name = "scope"
     count = 1
 }
